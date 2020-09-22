5be5238557d7e9e30cdb7fd14918c7cdda1e386e
amanuela97 <babyzewdie@gmail.com>
2020-09-22 16:09:04 +0300

    final commit

--- app/src/main/java/fi/metropolia/kari/arbasic1/MainActivity.kt
+++ app/src/main/java/fi/metropolia/kari/arbasic1/MainActivity.kt
@@ -1,41 +1,67 @@
 package fi.metropolia.kari.arbasic1
 
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.HitTestResult
 import com.google.ar.sceneform.assets.RenderableSource
 import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var fragment: ArFragment
-    private var testRenderable: ModelRenderable? = null
-    private val GLTF_ASSET = "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf"
+    private var sampleRenderable: Renderable? = null
+    private var bottleRenderable: ModelRenderable? = null
+    private var pikachuRenderable: ModelRenderable? = null
+    private var charmanderRenderable: ModelRenderable? = null
+    private var txtViewRenderable: ViewRenderable? = null
+    private var arrayV = ArrayList<View>()
+    private val GLTF_ASSET = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/WaterBottle/glTF/WaterBottle.gltf"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener { addObject() }
-
         fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
+        val bottle = bottle_iv
+        val pikachu = pikachu_iv
+        val charmander = charmander_iv
+        val hello = hello_tv
+        arrayV = arrayListOf(bottle, pikachu, charmander, hello)
+        arrayV.forEach { it.setOnClickListener(this)}
+        setUpModel()
 
-        /*val renderableFuture = ViewRenderable.builder()
-            .setView(this, R.layout.rendtext)
-            .build()
-        renderableFuture.thenAccept {it -> testRenderable = it }*/
 
+        fragment.setOnTapArPlaneListener { hitResult: HitResult?, plane: Plane?, motionEvent: MotionEvent? ->
+                    if (sampleRenderable == null) {
+                        return@setOnTapArPlaneListener
+                        }
+                    val anchor = hitResult!!.createAnchor()
+                    val anchorNode = AnchorNode(anchor)
+                    anchorNode.setParent(fragment.arSceneView.scene)
+                    val viewNode = TransformableNode(fragment.transformationSystem)
+                    viewNode.setParent(anchorNode)
+                    viewNode.renderable = sampleRenderable
+                    viewNode.select()
+                    }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setUpModel() {
         val modelUri = Uri.parse(GLTF_ASSET)
         val renderableFuture = ModelRenderable.builder()
             .setSource(
@@ -50,40 +76,74 @@ class MainActivity : AppCompatActivity() {
             )
             .setRegistryId(GLTF_ASSET)
             .build()
-        renderableFuture.thenAccept { testRenderable = it }
+        renderableFuture.thenAccept { bottleRenderable = it }
         renderableFuture.exceptionally { throwable -> Toast.makeText(
             applicationContext,
             "${throwable.printStackTrace()}",
             Toast.LENGTH_SHORT
         ).show()
             null
-         } //let user know what went wrong
+        } //let user know what went wrong
 
-
-        fragment.setOnTapArPlaneListener { hitResult: HitResult?, plane: Plane?, motionEvent: MotionEvent? ->
-                    if (testRenderable == null) {
-                        return@setOnTapArPlaneListener
-                        }
-                    val anchor = hitResult!!.createAnchor()
-                    val anchorNode = AnchorNode(anchor)
-                    anchorNode.setParent(fragment.arSceneView.scene)
-                    val viewNode = TransformableNode(fragment.transformationSystem)
-                    viewNode.setParent(anchorNode)
-                    viewNode.renderable = testRenderable
-                    viewNode.select()
-                    viewNode.setOnTapListener{ hitTestRes: HitTestResult?, motionEv: MotionEvent? ->
-                                button.visibility = View.INVISIBLE
-                            }
-
-                    }
+        val modelPikachu = Uri.parse("model.gltf")
+        val renderableFuture2 = ModelRenderable.builder()
+            .setSource(
+                this, RenderableSource.builder().setSource(
+                    this,
+                    modelPikachu,
+                    RenderableSource.SourceType.GLTF2
+                )
+                    .setScale(0.2f) // Scale the original model to 20%.
+                    .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                    .build()
+            )
+            .setRegistryId("model.gltf")
+            .build()
+        renderableFuture2.thenAccept { pikachuRenderable = it }
+        renderableFuture2.exceptionally { throwable -> Toast.makeText(
+            applicationContext,
+            "${throwable.printStackTrace()}",
+            Toast.LENGTH_SHORT
+        ).show()
+            null
         }
 
+        val modelCharmander = Uri.parse("model2.gltf")
+        val renderableFuture3 = ModelRenderable.builder()
+            .setSource(
+                this, RenderableSource.builder().setSource(
+                    this,
+                    modelCharmander,
+                    RenderableSource.SourceType.GLTF2
+                )
+                    .setScale(0.2f) // Scale the original model to 20%.
+                    .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                    .build()
+            )
+            .setRegistryId("model2.gltf")
+            .build()
+        renderableFuture3.thenAccept { charmanderRenderable = it }
+        renderableFuture3.exceptionally { throwable -> Toast.makeText(
+            applicationContext,
+            "${throwable.printStackTrace()}",
+            Toast.LENGTH_SHORT
+        ).show()
+            null
+        }
 
-        private fun addObject() {
+        ViewRenderable.builder()
+            .setView(this, R.layout.hello)
+            .build().thenAccept { txtViewRenderable = it }
+
+
+    }
+
+
+       private fun addObject() {
             val frame = fragment.arSceneView.arFrame
             val pt = getScreenCenter()
             val hits: List<HitResult>
-            if (frame != null && testRenderable != null) {
+            if (frame != null && sampleRenderable != null) {
                 hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
                 for (hit in hits) {
                     val trackable = hit.trackable
@@ -93,14 +153,26 @@ class MainActivity : AppCompatActivity() {
                         anchorNode.setParent(fragment.arSceneView.scene)
                         val mNode = TransformableNode(fragment.transformationSystem)
                         mNode.setParent(anchorNode)
-                        mNode.renderable = testRenderable
+                        mNode.renderable = sampleRenderable
                         mNode.select()
                         break
                     } } } }
 
-         private fun getScreenCenter(): android.graphics.Point {
+        private fun getScreenCenter(): android.graphics.Point {
             val vw = findViewById<View>(android.R.id.content).rootView
             return android.graphics.Point(vw.width / 2, vw.height / 2)
+        }
+
+
+        override fun onClick(p0: View?) {
+            when(p0?.id){
+                bottle_iv.id -> sampleRenderable = bottleRenderable
+                pikachu_iv.id -> sampleRenderable = pikachuRenderable
+                charmander_iv.id  -> sampleRenderable = charmanderRenderable
+                hello_tv.id -> sampleRenderable = txtViewRenderable
+            }
+            addObject()
+            Log.i("DBG", "${p0}")
         }
 
     }
